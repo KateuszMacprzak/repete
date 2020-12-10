3bcdeeffa57d59872a3dd0106606390f276a766e
Mateusz@MSI <matkac98@gmail.com>
2020-12-10 21:47:51 +0100

    Jazda jazda biała gwiazda

--- powtorka.py
+++ powtorka.py
@@ -3,5 +3,7 @@ print('Welcome in program %s '%name)
 print("How you doing today from 1 to 10 ? ")
 value=input("Enter the range: ")
 value=int(value)
+x='y'
 if (value==1 or value==2 or value==3 ):
-    x=input("So, you had bad day. Want to play the game? Enter y for yes or n for no.... ")
+    print("So, you had bad day.)
+    x=input
