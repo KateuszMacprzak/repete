c98d88cae73676b9cedd1d315f582fbc5959ddf4
Mateusz@MSI <matkac98@gmail.com>
2020-12-10 21:44:20 +0100

    Pierwsza komenda
    Dodanie pierwszej komendy zachęcającej użytkownika do gry

--- powtorka.py
+++ powtorka.py
@@ -1,1 +1,7 @@
-print('Hello world!')
+name=input("Enter your name: ")
+print('Welcome in program %s '%name)
+print("How you doing today from 1 to 10 ? ")
+value=input("Enter the range: ")
+value=int(value)
+if (value==1 or value==2 or value==3 ):
+    x=input("So, you had bad day. Want to play the game? Enter y for yes or n for no.... ")
